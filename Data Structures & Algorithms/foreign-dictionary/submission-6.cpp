6cd5854f321e31d06b7a15a9c03d23d02b8a9883
kartik garg <74829066+kartik2710@users.noreply.github.com>
2026-09-10 21:08:47 +0530

    Add: foreign-dictionary - submission-6

---
+++ Data Structures & Algorithms/foreign-dictionary/submission-6.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    string foreignDictionary(vector<string>& words) {
+         
+         vector<int> present(26,0), inorder(26,0);
+         vector<int> adj[26];
+         for(auto x: words)
+         {
+            string str=x;
+            for(int i=0; i<str.size(); i++)
+            present[str[i]-'a']=1;
+         }
+
+
+         for(int i=0; i<words.size()-1; i++)
+         {
+             string a=words[i], b=words[i+1];
+
+             if(a.size()>b.size() &&  a.substr(0,b.size())==b) 
+             return "";
+
+             int j=0,k=0;
+
+             while(j<a.size() && k<b.size())
+             {
+                if(a[j]!=b[k])
+                {
+                    adj[a[j]-'a'].push_back(b[k]-'a');
+                    inorder[b[k]-'a']++;
+                    break;
+                }
+                j++,k++;
+             }
+
+         }
+
+         queue<int> q;
+
+         for(int i=0; i<26; i++)
+         if(present[i] && inorder[i]==0) q.push(i);
+
+         string ans="";
+
+         while(!q.empty())
+         {
+            int node=q.front();
+           // cout<<node<<"."<<char(node+'a')<<" ";
+             ans+=char(node+'a');
+             q.pop();
+             for(auto x: adj[node])
+             {
+                inorder[x]--;
+                if(inorder[x]==0) q.push(x);
+             }
+         }
+
+         int sum=0;
+         for(int i=0; i<present.size(); i++)
+         if(present[i]) sum++;
+          
+        //  cout<<sum<<endl;
+         return sum==ans.size()? ans: "";
+
+
+
+
+    }
+};
