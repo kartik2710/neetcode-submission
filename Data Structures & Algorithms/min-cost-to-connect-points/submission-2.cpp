001a158f36cc315e77b71325e6537a04f6d47d1d
kartik garg <74829066+kartik2710@users.noreply.github.com>
2026-09-19 16:15:21 +0530

    Add: min-cost-to-connect-points - submission-2

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+   int manH(vector<int> &a, vector<int> &b)
+   {
+      return abs(a[0]-b[0])+ abs(a[1]-b[1]);
+   }
+    int minCostConnectPoints(vector<vector<int>>& points) {
+         int n = points.size();
+        vector<int> visited(n,0);
+
+        
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>,
+         greater<pair<int,int>> > pq;
+         pq.push({0,0});
+         int ans=0;
+
+         while(!pq.empty())
+         {
+            int value=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
+
+            if(visited[node]) continue;
+             ans+=value;
+
+            visited[node]=1;
+
+            for(int i=0; i<n; i++)
+            {
+                if(visited[i]==0)
+                {
+                    int dist=manH(points[i], points[node]);
+                    pq.push({dist,i});
+                }
+            }
+         }
+
+         return ans;
+    }
+};
